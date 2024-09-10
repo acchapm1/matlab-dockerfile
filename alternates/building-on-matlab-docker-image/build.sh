0ac1ad3fdd6f26a70cda0e20e62947b4866e7685
Alan Chapman <acchapm1@asu.edu>
2024-09-10 14:52:22 -0700

    adding build.sh script

---
+++ alternates/building-on-matlab-docker-image/build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+
+# docker build -t matlab_with_add_ons:R2024a --build-arg LICENSE_SERVER=27000@MyServerName .
+
+# Here is an example docker build command with the optional build arguments.
+docker build --build-arg MATLAB_RELEASE=R2024a \
+             --build-arg MATLAB_PRODUCT_LIST="MATLAB_Support_Package_for_Quantum_Computing" \
+             --build-arg MATLAB_INSTALL_LOCATION="/opt/matlab/R2024a" \
+             --build-arg LICENSE_SERVER=27000@PubKeyServer7.asu.edu \
+             -t asu-matlab-quantum .
